a93a1a370db51d2e2e8c56805e0366bd9c880895
Bezhenar Yury <yurryn@gmail.com>
2012-12-14 01:25:15 +0200

    deploy.rb edited

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,40 @@
 set :application, "Rynda"
-set :repository,  "set your repository location here"
+#set :repository,  "ssh://git@github.com/vivakalman/Rynda.git"
+set :repository,  "git@github.com:vivakalman/Rynda.git"
+set :scm, 'git'
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :user, 'yurry'
+set :branch, "master"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :git_shallow_clone, 1
+set :use_sudo, false
+set :deploy_to, "/home/yurry/Rynda/public/#{application}"
+set :deloy_via, :remote_cache
+set :keep_releases, 1
+set :rails_env, "production"
+set :migrate_target, :latest
+ 
+default_run_options[:pty] = true
+#ssh_options[:forward_agent] = true - с этим выдаёт ошибку: Error reading response length from authentication socket
+
+role :web, "192.168.88.5"                          # Your HTTP server, Apache/etc
+role :app, "192.168.88.5"                          # This may be the same as your `Web` server
+role :db,  "192.168.88.5", :primary => true # This is where Rails migrations will run
+
+ namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
+ 
+ namespace :deploy do
+   desc "Recreate symlink"
+   task :resymlink, :roles => :app do
+     run "rm -f #{current_path} && ln -s #{release_path} #{current_path}"
+   end
+ end
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
